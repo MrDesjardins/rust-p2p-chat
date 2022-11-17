cfaaf74892ab2bbfa007fd5a9683d751069038bd
Patrick <mrdesjardins@gmail.com>
2022-11-16 20:06:48 -0800

    - Debug for VSCode - Loop that get user inputs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-
+use std::io;
+use std::io::Write;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)] // Read from `Cargo.toml`
 struct Cli {
@@ -13,5 +14,22 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    println!("Rust P2P running on address {}:{} with username {}", args.ip, args.port, args.username);
+    println!(
+        "Rust P2P running on address {}:{} with username {}\n",
+        args.ip, args.port, args.username
+    );
+
+    loop {
+        let mut user_input = String::new();
+
+        print!("> ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut user_input).unwrap();
+        let user_input_clean = user_input.trim();
+        match user_input_clean {
+            "exit" => break,
+            "/name" => println!("Changing your name"),
+            _ => println!("Command [{}] unrecognized", user_input_clean),
+        }
+    }
 }
